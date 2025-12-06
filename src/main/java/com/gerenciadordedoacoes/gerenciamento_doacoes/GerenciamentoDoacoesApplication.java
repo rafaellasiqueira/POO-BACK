27f6ce5841380df7a0308f7b292ec6b0104d1f04
rafaellasiqueira <rafaellafelipe2301@gmail.com>
2025-12-06 12:45:19 -0300

    Primeira versão

---
+++ src/main/java/com/gerenciadordedoacoes/gerenciamento_doacoes/GerenciamentoDoacoesApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciadordedoacoes.gerenciamento_doacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDoacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDoacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerenciadordedoacoes/gerenciamento_doacoes/GerenciamentoDoacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciadordedoacoes.gerenciamento_doacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDoacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
